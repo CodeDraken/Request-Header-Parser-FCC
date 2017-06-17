2e3318156defb03aa3e9a5f337c4b5a7b85666bb
CodeDraken <codedraken@gmail.com>
2017-06-16 19:12:52 -0500

    IP fix

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.use(cors());
 app.use(useragent.express());
 
 app.get('/api/whoami', (req, res) => {
-  let address = req.ip,
+  let address = request.headers['x-forwarded-for'] || request.connection.remoteAddress, // req.ip
     language = req.acceptsLanguages(),
     system = req.useragent; // req.get('User-Agent')
 
   let { browser, os, version } = system;
 
-  res.json({ address, language, browser, os, 'browserVersion': version });
+  res.json({ system, address, language, browser, os, 'browserVersion': version });
 });
 
 app.listen(port, () => {
